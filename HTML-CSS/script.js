0c79683a1015a411442555067e958fc610592996
Mushraf667 <mushrafmushu855@gmail.com>
2026-06-11 14:10:44 +0530

    HTML CSS

---
+++ HTML-CSS/script.js
@@ -0,0 +1,74 @@
+/*let name="Mushraf";
+var age=21;
+const height="5.8";
+console.log(age);
+console.log(age+5);
+console.log(age-3);
+console.log(age*2);
+console.log(age/2);
+console.log(age%4);
+age+=3;
+console.log(age);
+age=21
+let age1=25;
+console.log(age>age1);
+console.log(age<age1);
+console.log(age>=age1);
+console.log(age<=age1);
+console.log(age==age1);
+console.log(age!=age1);
+let food1 = "Dosa";
+let break_fast="Dosa";
+let food2="upma";
+let food3="pori";
+let food4="idli";
+if(food1==break_fast){
+    console.log("Its nice to have Dosa in the morning");
+}
+else{
+    console.log("I prefer Dosa for breakfast");
+let n=3800;
+if(n>=500){
+    let fh=Math.floor(n/500);
+    console.log("500: "+fh);
+    n=n%500;
+}
+if(n>=200){
+    let fh=Math.floor(n/200);
+    console.log("200: "+fh);
+    n=n%200;
+}
+if(n>=100){
+    let fh=Math.floor(n/100);
+    console.log("100: "+fh);
+    n=n%100;
+}
+let user_name="Mushraf";
+let password="padhu143";
+let name="Shaik_Mushraf";
+let pin="mushu123";
+let name_1="Fazil";
+let pin_1="fazil123";
+if(name=="Shaik_Mushraf"){
+    console.log(user_name);
+    if(pin=="mushu123"){
+        console.log("Welcome to your account");
+    }
+}
+for(let i=1;i<=5;i++){
+    if(i%2!=0){
+        console.log(i+"received a gift");
+    }
+}
+for(let i=1;i<=5;i++){
+    if(i%2==0){
+        console.log(i+"received a gift");
+    }
+}*/
+function cook(){
+    console.log("Pour the flour in tava");
+    console.log("Rub it round order with gareta");
+    console.log("spill some oil above it then wait");
+    console.log("Twist it with dosa gareta");
+    console.log("Atleast gently serve it to Mushraf")
+}
